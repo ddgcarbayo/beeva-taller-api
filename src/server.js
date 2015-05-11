e5e3e27c47ea55e971aa59d4085a8a8073a7e186
beeva-davidgarcia <david.garcia@beeva.com>
2015-05-11 09:04:45 +0200

    Ej. 4

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ var app = express();
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var device  = require('express-device');
+var bodyParser = require('body-parser');
 
 var runningPortNumber = process.env.PORT;
 
@@ -21,6 +22,26 @@ app.configure(function(){
 	//set the view engine
 	app.set('view engine', 'ejs');
 	app.set('views', __dirname +'/views');
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    app.use(function(req, res, next) {
+        req.rawBody = '';
+        req.setEncoding('utf8');
+
+        req.on('data', function(chunk) {
+            req.rawBody += chunk;
+        });
+
+        req.on('end', function() {
+            if(req.rawBody){
+                var body = JSON.parse(req.rawBody);
+                req.body=body;
+            }
+            next();
+        });
+    });
 
 	app.use(device.capture());
 });
@@ -45,10 +66,18 @@ app.get("/object", function(req, res){
     res.json(objetoChat);
 });
 
+app.post("/webhook", function(req, res){
+    var user = req.body.user;
+    var repo = req.body.repository.name;
+    objetoChat.push('<span style="">El usuario '+user+' ha hecho commit en el respositorio '+repo+'</span>');
+    io.sockets.emit('update', objetoChat);
+    res.json({result : 200});
+});
+
 
 io.sockets.on('connection', function (socket) {
 
-    objetoChat.unshift("<span style=\"color:red !important\">someone connected</span>");
+    objetoChat.unshift("<span style=\"color:red !important\">Nuevo cliente conectado</span>");
 	io.sockets.emit('update', objetoChat);
 
 	socket.on('update', function(data, fn){
