973b9bae9340d0e10be7895f2c29d243625bd3dc
Manuel Eusebio de Paz Carmona <manuel.depaz@beeva.com>
2015-05-12 12:42:04 +0200

    updating rest

--- src/app.js
+++ src/app.js
@@ -1,24 +1,17 @@
 var app = angular.module("myApp", ['nvd3ChartDirectives']);
 
-app.controller("myNoteCtrl", function($scope,$http) {
+app.controller("myChartCtrl", function($scope,$http) {
 
-    $scope.cheese = [];
-    $scope.bar = [];
+    $scope.data = {};
+    $scope.data.bar = [];
 
     $http.get('http://localhost:3001/data?type=cheese').
         success(function(data, status, headers, config) {
-            $scope.data.cheese = data;
+            $scope.data.bar = data;
         }).
         error(function(data, status, headers, config) {
             console.log('error');
+            $scope.bar = [];
         });
 
-
-    $http.get('http://localhost:3001/data?type=bar').
-        success(function(data, status, headers, config) {
-            $scope.data.bar = {};
-        }).
-        error(function(data, status, headers, config) {
-            console.log('error');
-        });
 });
