8640afd0412820f2905c000ee6510e19eb3011b5
David García Carbayo <david.garcia@beeva.com>
2015-05-12 11:14:11 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
-var device  = require('express-device');
 var bodyParser = require('body-parser');
 
 var runningPortNumber = process.env.PORT;
