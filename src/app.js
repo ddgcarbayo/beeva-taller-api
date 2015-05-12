d95e740f143511cc0849d6f53cb73141b74e1dae
Manuel Eusebio de Paz Carmona <manuel.depaz@beeva.com>
2015-05-12 12:47:17 +0200

    Ajustando puertos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.controller("myChartCtrl", function($scope,$http) {
     $scope.data = {};
     $scope.data.bar = [];
 
-    $http.get('http://localhost:3001/data?type=cheese').
+    $http.get('http://localhost:8080/data?type=cheese').
         success(function(data, status, headers, config) {
             $scope.data.bar = data;
         }).
--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.get('/', function(req, res) {
 });
 
 
-var server = app.listen(3001, function () {
+var server = app.listen(8080, function () {
 
     var host = server.address().address;
     var port = server.address().port;
